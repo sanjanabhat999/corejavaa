5bd83651f16daee7d6acc487bb0621353df96211
sanjanabhat999 <41315070+sanjanabhat999@users.noreply.github.com>
2018-07-19 12:14:42 +0530

    emp1

--- src/firstproject/Emp1.java
+++ src/firstproject/Emp1.java
@@ -2,4 +2,50 @@ package firstproject;
 
 public class Emp1 {
 
+
 }
+
+class HR extends Emp1{
+	
+}
+ class Recruiter extends HR{
+	 
+ }
+ 
+ class Financier extends HR{
+	 
+ }
+ 
+ class Technical extends Emp1{
+	 
+ }
+ 
+ class Javapm extends Technical{
+	 
+ }
+ 
+ class Mainframepm extends Technical{
+	 
+ }
+ 
+ class Typecasting{
+ public static void main(String[] args)
+ {    
+	 Technical t= new Javapm();
+	 HR h=new Recruiter();
+	 Emp1 e =new HR();
+	 Emp1 e1=new Mainframepm();
+//	    downcasting
+	 
+	 Technical t1=new Javapm();
+	 if(t1 instanceof Javapm ) {
+		 Javapm jp=(Javapm)t1;
+	 }
+	 
+	 
+	 
+	 
+	 
+ }
+ 
+ }
