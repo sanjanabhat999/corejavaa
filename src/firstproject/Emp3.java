e86dc8b4d3b0e8cbddaeebe624ef7b3da12eb127
sanjanabhat999 <41315070+sanjanabhat999@users.noreply.github.com>
2018-07-19 14:58:57 +0530

    prac

---
+++ src/firstproject/Emp3.java
@@ -0,0 +1,18 @@
+package firstproject;
+
+public class Emp3 {
+	String name;
+	Emp3(String nm)
+	{
+		name=nm;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Emp3 e=new Emp3("jhansi");
+		System.out.println(e.name);
+
+	}
+
+}
+
