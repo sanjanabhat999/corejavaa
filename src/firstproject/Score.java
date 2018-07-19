83f7f910b0c0f2e7bf473e16c255fa40377508f4
sanjanabhat999 <41315070+sanjanabhat999@users.noreply.github.com>
2018-07-19 13:33:57 +0530

    percentage

---
+++ src/firstproject/Score.java
@@ -0,0 +1,16 @@
+package firstproject;
+
+public class Score {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m1=95;
+		int m2=45;
+		int m3=88;
+	    double percentage;
+	    percentage= (m1+m2+m3)*2.5;
+	    System.out.println(percentage);
+
+	}
+
+}
